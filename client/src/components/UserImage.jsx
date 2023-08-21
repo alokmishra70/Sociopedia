13d9f1116f6c4c1339c8b450458695e9cc2b32be
Alok Kumar Mishra <alok99893@gmail.com>
2023-08-21 22:59:11 +0530

    change http to https

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://sociopedia-server-crzw.onrender.com/assets/${image}`}
+        src={`https://sociopedia-server-crzw.onrender.com/assets/${image}`}
       />
     </Box>
   );
--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://sociopedia-server-crzw.onrender.com/users/${userId}/friends`,
+      `https://sociopedia-server-crzw.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
